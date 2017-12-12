585e41bd381a97c8a360207ae7ca6593114e612d
Mounic <mounicraju@gmail.com>
2017-12-12 11:48:05 +0800

    API class defined to get table data for selected country and location
    identified end point and defined API class

--- scrape.py
+++ scrape.py
@@ -26,7 +26,29 @@ class Extract_table:
                 self.Data[key].append([cell.text for cell in row("td")])
         return self.Data
 
+class API(object):
+    """API to get a country"""
+    def __init__(self, BASE_URL, Country, city=0):
+        self.base = BASE_URL
+        self.url = BASE_URL+"country_result.jsp?country="+Country
+        self.country = Country
+        self.result = {}
+        responce = self.get_page(self.url)
+        if responce:
+            self.page = BeautifulSoup(responce.text, "html")
+            self.get_city()
+            EX = Extract_table(self.page)
+            self.result[Country] = EX.extract()
+            if city:
+                self.get_all_city(Country)
+        else:
+            self.page = None
+            self.city = None
+
 if __name__ == "__main__":
     COUNTRY = ['Malaysia']
     results = {} 
-    
+    city = 0 #set 1 to crawl all city
+    for i in COUNTRY:
+        obj = API(URL, i, city)
+        results[i] = obj.get_result()
